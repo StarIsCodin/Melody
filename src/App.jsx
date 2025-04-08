2435ee7a5f747b692a680164b1a65007fcaca6d7
star-code1 <EmailGitHub>
2025-04-08 12:19:32 +0700

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,55 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom'; 
-import { Row, Col } from 'react-bootstrap';
-import Sidebar from './components/Sidebar';
-import Page from './components/Page';
-import Header from './components/Header'
-import Player from './components/Player'
-import { MusicPlayerProvider } from './contexts/MusicPlayerContext';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import Sidebar from "./components/Sidebar";
+import Page from "./components/Page";
+import Header from "./components/Header";
+import Player from "./components/Player";
+import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import SidebarToggle from "./components/SidebarToggle";
 
 function App() {
-  
-  if (process.env.NODE_ENV === 'production') disableReactDevTools()
+  if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
-    <BrowserRouter basename="/"> 
-    <MusicPlayerProvider>
-      <div className='container-fluid vh-100' >
-        <Row>
-          <Col style={{ flex: "0.25", padding: "0" }}>
-            <Sidebar />
-          </Col>
-          <Col  style={{padding: "0"}}>
-            <Header />
-            <Page />
-          </Col>
-        </Row>
-      </div>
-      <Player></Player>
+    <BrowserRouter basename="/">
+      <MusicPlayerProvider>
+        <div className="app-container">
+          <SidebarToggle isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+
+          <div className="content-container">
+            <div className={`sidebar ${sidebarOpen ? "active" : ""}`}>
+              <Sidebar />
+            </div>
+
+            <div
+              className={`sidebar-overlay ${sidebarOpen ? "active" : ""}`}
+              onClick={() => setSidebarOpen(false)}
+            ></div>
+
+            <div className="main-content col">
+              <Header />
+              <Page />
+            </div>
+          </div>
+
+          <div className="player">
+            <Player />
+          </div>
+        </div>
       </MusicPlayerProvider>
     </BrowserRouter>
   );
 }
+
 export default App;
 
